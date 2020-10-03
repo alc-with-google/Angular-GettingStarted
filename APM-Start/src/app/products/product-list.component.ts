64ed5f9bb27e3d00e0e9540077540fdeb112fbef
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2020-10-03 22:06:45 +0100

    Lesson: About Interpolation and Directives

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'pm-products', // this means the component can be used a directive, <pm-products></pm-products> providing access to the html
+  templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+  // interpolation (one-way binding) is a binding technique
+  // it's about communication between class and template
+  // class to template
+  // here, pageTitle is defined which can used in the template as {{pageTitle}}
+  // Or, <h1 innertText={{pageTitle}}></h1>, {{'Title: ' + pageTitle}}
+  // used with class methods as well: {{'Title: ' + getTitle()}}
+  pageTitle: string = 'The Product List';
+  products: any[] = [
+    {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2019",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    },
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2019",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    }
+  ];
+}
